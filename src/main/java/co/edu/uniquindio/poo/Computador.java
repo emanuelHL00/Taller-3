061235fde312635a99dcdd0b96a4ab441d82b28e
emanuelHL00 <emanuel.hernandezl@uqvirtual.edu.co>
2025-10-14 17:37:21 -0500

    update

--- src/main/java/co/edu/uniquindio/poo/Computador.java
+++ src/main/java/co/edu/uniquindio/poo/Computador.java
@@ -1,4 +1,44 @@
 package co.edu.uniquindio.poo;
 
 public class Computador {
+    private String ram, cpu, discoDuro, gpu;
+
+    private Computador(String ram, String cpu, String discoDuro, String gpu){
+        this.ram = ram;
+        this.cpu = cpu;
+        this.discoDuro = discoDuro;
+        this.gpu = gpu;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    public String getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public String getDiscoDuro() {
+        return discoDuro;
+    }
+
+    public void setDiscoDuro(String discoDuro) {
+        this.discoDuro = discoDuro;
+    }
+
+    public String getGpu() {
+        return gpu;
+    }
+
+    public void setGpu(String gpu) {
+        this.gpu = gpu;
+    }
 }
